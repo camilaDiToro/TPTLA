5ab04031a1aa4d2765d7d2dce9052f2fd1535980
camilaDiToro <camiladitoro@gmail.com>
2022-06-15 18:12:03 -0300

    fix run script

--- run.sh
+++ run.sh
@@ -8,10 +8,20 @@
         cd ./EX0$1
         ./EX0$1.sh
         mv out.html ../../
-    elif [ "$1" -ge "10" ] && [ "$1" -le "18" ]
+    elif [ "$1" -eq "10" ]
         then 
             cd ./EX$1
             ./EX$1.sh
+            mv out.html ../../
+    elif [ "$1" -ge "11" ] && [ "$1" -le "14" ]
+        then 
+            cd ./EX$1
+            ./EX$1.sh
+    elif [ "$1" -ge "15" ] && [ "$1" -le "18" ]
+        then 
+            cd ./EX$1
+            ./EX$1.sh
+            mv out.html ../../
     else
         echo "The example number must be between 1 and 16."
     fi
